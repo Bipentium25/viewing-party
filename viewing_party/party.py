a7eb6aabf61fe94feb00001d0d91a007ee26b4ed
Qiusi Agre <qiusi.agre@icloud.com>
2025-09-22 15:51:42 -0400

    finish the test_wave_01
    1. implement thefunctions in party.py
    2. add some assertions in test_wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,50 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = {}
+
+    # those three attributes are truthy
+    if not (title and genre and rating):
+        return None
+    
+    if title and genre and rating:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+    
+    return movie
+
+def add_to_watched(user_data, movie):
+    # empty list is a valid value
+    # user_data = {"watched": [{},{},{}]}
+
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    # user_data = {"watchlist": [{},{},{}]}
+
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    # should we add a test to reply the movie which is
+    # not in watchlist that watched by user? like adding
+    # to the watched list?
+
+    for index_dic in range(len(user_data["watchlist"])):
+        if title == user_data["watchlist"][index_dic]["title"]:
+            user_data["watched"].append(user_data["watchlist"][index_dic])
+            user_data["watchlist"].pop(index_dic)
+
+    return user_data
+    
+
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
