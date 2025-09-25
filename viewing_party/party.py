95d06cfc8d11d0e44d384f484c3b0fd14c1582d7
Qiusi Agre <qiusi.agre@icloud.com>
2025-09-25 14:03:19 -0400

    wave-03

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -62,7 +62,7 @@ def get_watched_avg_rating(user_data):
 
 def get_most_watched_genre(user_data):
     genere_dict = {}
-    
+
     if not user_data["watched"]:
         return None
     for i in range(len(user_data["watched"])):
@@ -84,16 +84,110 @@ def get_most_watched_genre(user_data):
 
     # return most_popular_genre
 
- 
 
-    
+# # USER_DATA_2b = {
+#     "watched": [
+#         INTRIGUE_1,
+#         FANTASY_2,
+#         ACTION_1,
+#         {
+#     "title": "The Lord of the Functions: The Fellowship of the Function",
+#     "genre": "Fantasy",
+#     "rating": 4.8
+# },
+#         FANTASY_3,
+#         INTRIGUE_2,
+#     ],
+#     "friends": [
+#         {
+#             "watched": [
+#                 { "title": "The Lord of the Functions: The Fellowship of the Function",
+#                      "genre": "Fantasy",
+#                         "rating": 4.8, 
+#                           "host" = "netflix"},
+#                 FANTASY_3,
+#                 FANTASY_4,
+#                 HORROR_1,
+#             ]
+#         },
+#         {
+#             "watched": [
+#                 FANTASY_1,
+#                 ACTION_1,
+#                 INTRIGUE_1,
+#                 INTRIGUE_3,
+#             ]
+#         }
+#     ]  
+# }
 
+# def get_unique_watched(user_data):
+#     unique_watched = []
+#     duplicate = False
+#     for i in range(len(user_data["watched"])):
+#         duplicate = False
+#         # if item is in set(t1, t2, t3):
+#         for n in range(len(user_data["friends"])):
+#             for m in range(len(user_data["friends"][n]["watched"])):
+#                 if user_data["watched"][i]["title"] == user_data["friends"][n]["watched"][m]["title"]:
+#                     duplicate = True
+#                     break
+#             if duplicate:
+#                 break
+#         if not duplicate: 
+#             unique_watched.append(user_data["watched"][i])
 
+#     # if  user_data["watched"][i]["title"] != user_data["friends"][n]["watched"][m]["title"]:    
+#     return   unique_watched
+def get_title_friends_watched(user_data):
+    movie_titile_friends_watched = set()
 
+    for d in user_data["friends"]:
+        for d_movie in d["watched"]:
+            movie_titile_friends_watched.add(d_movie["title"])
 
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
+    return movie_titile_friends_watched
+
+def get_unique_watched(user_data):
+    unique_watched = []
+
+    for movie in (user_data["watched"]):
+        if movie["title"] not in get_title_friends_watched(user_data):
+            unique_watched.append(movie)
+
+    return   unique_watched
+
+def get_friends_unique_watched(user_data):
+    friends_unique_watched = []
+    set_movie_title = get_title_friends_watched(user_data)
+
+    for movie in (user_data["watched"]):
+        if movie["title"] in set_movie_title:
+            set_movie_title.remove(movie["title"])
+
+    for d in user_data["friends"]:
+        for d_movie in d["watched"]:           
+                if d_movie["title"] in set_movie_title and d_movie not in friends_unique_watched:
+                    friends_unique_watched.append(d_movie)
+
+    return   friends_unique_watched
+
+# def get_friends_unique_watched(user_data):
+#     friends_unique_watched = []
+#     duplicate = False
+#     for movie in (user_data["watched"]):
+#         duplicate = False
+#         for n in range(len(user_data["friends"])):
+#             for m in range(len(user_data["friends"][n]["watched"])):
+#                 if user_data["friends"][n]["watched"][m]["title"] == movie["title"]:
+#                     duplicate = True
+#                     break
+#             if duplicate:
+#                 break
+#         if not duplicate: 
+#             friends_unique_watched.append(user_data["friends"][n]["watched"][m])
+
+#     return   friends_unique_watched
 
         
 # -----------------------------------------
